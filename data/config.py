71d9f2adc5640ddc0750f726e185b3f147139e62
mkadir <muhammadalihakimov03@gmail.com>
2024-07-01 12:02:52 +0500

    configurations for

--- data/config.py
+++ data/config.py
@@ -11,7 +11,6 @@ ADMINS = env.list("ADMINS")
 SECRET_KEY = env.str('SECRET_KEY')
 WEB_ADMINS = env.list("WEB_ADMINS")
 WEB_PASSWORD = env.list("WEB_PASSWORD")
-quran_icon = 'https://cdn-icons-png.flaticon.com/512/6565/6565365.png'
 auth_users = dict(map(lambda x: (x[0], x[1]), zip(WEB_ADMINS, WEB_PASSWORD)))
 CHANNELS = []
 
